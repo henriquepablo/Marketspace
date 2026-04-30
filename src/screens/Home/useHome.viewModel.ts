e5459d70ef59b9553580876394522f3855d1c99b
henriquepablo <pablo269704@gmail.com>
2026-04-30 08:05:01 -0300

    feat: add states on function reset filters

--- src/screens/Home/useHome.viewModel.ts
+++ src/screens/Home/useHome.viewModel.ts
@@ -10,13 +10,13 @@ export enum PaymentMethod {
 
 export const useHomeViewModel = () => {
 
-    const [ payments, setPayments ] = useState<PaymentMethod[]>([]);
-    const [ openModalFilter, setOpenModalFilter ] = useState<boolean>(false);
-    const [ switchChange, setSwitchChange ] = useState<boolean>(false);
-    const [ condition, setCondition ] = useState<'new' | 'used'>('new');
+    const [payments, setPayments] = useState<PaymentMethod[]>([]);
+    const [openModalFilter, setOpenModalFilter] = useState<boolean>(false);
+    const [switchChange, setSwitchChange] = useState<boolean>(false);
+    const [condition, setCondition] = useState<'new' | 'used'>('new');
 
     function togglePayment(type: PaymentMethod) {
-        
+
         setPayments(prev =>
             prev.includes(type)
                 ? prev.filter(p => p !== type)
@@ -26,9 +26,11 @@ export const useHomeViewModel = () => {
 
     const resetFilters = () => {
         setPayments([]);
+        setSwitchChange(false);
+        setCondition('new');
     }
 
-    return { 
+    return {
         payments, togglePayment, openModalFilter, setOpenModalFilter, resetFilters, switchChange, setSwitchChange, condition, setCondition
     }
 }
